b32c645929f4d969836e9ebdf8aedc884db80a43
Alexane Kurek <alexane.kurek7@gmail.com>
2023-05-23 13:50:01 -0400

    on the checkbox

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #Read all
+  get 'tasks', to:'tasks#index'
+
+  #Create a new task
+  get 'tasks/new', to:'tasks#new', as:'new_task'
+  post 'tasks', to:'tasks#create'
+
+
+  #Read one
+  get 'tasks/:id', to:'tasks#show', as:'task'
+
+  #Edit
+  get 'task/:id/edit', to: 'tasks#edit', as:'edit_task'
+  patch 'task/:id', to: 'tasks#update'
 end
